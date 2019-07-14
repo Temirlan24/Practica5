91d53d986fda695a091a7a4da80aae5bbb4fded1
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-14 16:02:08 +0600

    Сделал Практику 5. Создал класс и сделал перегрузку операторов.

---
+++ ConsoleApp1/ConsoleApp1/ClassArray.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ClassArray
+    {
+        public string stroka { get; set; }
+        public int[] array { get; set; }
+        public int sizeArray { get; set; }
+        public ClassArray()
+        {
+        }
+        public ClassArray(string stroka)
+        {
+            this.stroka = stroka;
+        }
+        public ClassArray(int[] array, int sizeArray)
+        {
+            this.sizeArray = sizeArray;
+            for (int i = 0; i < sizeArray; i++)
+            {
+                this.array[i] = array[i];
+            }
+        }
+        public void ReadArray(int[] array, int sizeArray)
+        {
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < sizeArray; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+        }
+        public void ShowArray(int[] array)
+        {
+            Console.WriteLine("Элементы массива:");
+            foreach (var element in array)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+        public static bool operator >(ClassArray firstArray, ClassArray secondArray)
+        {
+            int sumFirstArray = 0, sumSecondArray = 0;
+            for (int i = 0, j = 0; i < firstArray.sizeArray && j < secondArray.sizeArray; i++, j++)
+            {
+
+                sumFirstArray += firstArray.array[i];
+                sumSecondArray += secondArray.array[i];
+            }
+            return (sumFirstArray > sumSecondArray);
+        }
+        public static bool operator <(ClassArray firstArray, ClassArray secondArray)
+        {
+            int sumFirstArray = 0, sumSecondArray = 0;
+            for (int i = 0, j = 0; i < firstArray.sizeArray && j < secondArray.sizeArray; i++, j++)
+            {
+
+                sumFirstArray += firstArray.array[i];
+                sumSecondArray += secondArray.array[i];
+            }
+            return (sumFirstArray < sumSecondArray);
+        }
+
+        public static bool operator ==(ClassArray firstSroka, ClassArray secondStroka)
+        {
+            return (firstSroka.stroka == secondStroka.stroka);
+        }
+        public static bool operator !=(ClassArray firstSroka, ClassArray secondStroka)
+        {
+            return (firstSroka.stroka != secondStroka.stroka);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray = 5;
+            int[] arrayFirst = new int[sizeArray];
+            int[] arraySecond = new int[sizeArray];
+            ClassArray objectFirst = new ClassArray();
+            objectFirst.stroka = "sfsdfs";
+            ClassArray objectSecond = new ClassArray();
+            objectSecond.stroka = "sfsdfs";
+            if (objectFirst == objectSecond)
+            {
+                Console.WriteLine("Equal");
+            }
+            else if(objectFirst != objectSecond)
+            {
+                Console.WriteLine("Not Equal");
+            }
+
+            objectFirst.ReadArray(arrayFirst, sizeArray);
+            objectFirst.ShowArray(arrayFirst);
+            Console.WriteLine();
+            objectSecond.ReadArray(arraySecond, sizeArray);
+            objectSecond.ShowArray(arraySecond);
+            Console.WriteLine();
+            if (objectFirst > objectSecond)
+            {
+                Console.WriteLine("Sum of first array greater than sum of second array");
+            }
+            else
+            {
+                Console.WriteLine("Sum of first array not greater than sum of second array");
+            }
+            if (objectFirst < objectSecond)
+            {
+                Console.WriteLine("Sum of first array less than sum of second array");
+            }
+            else
+            {
+                Console.WriteLine("Sum of first array not less than sum of second array");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
